48e46ba49397020551f5bb3a166d31ec9246bed3
Elí Maciel <tecno_eli@hotmail.com>
2023-12-07 14:33:58 -0600

    checkpoint

--- src/components/canvas-screen.ts
+++ src/components/canvas-screen.ts
@@ -3,7 +3,24 @@ import { CustomComponent, register } from "../lib/CustomComponent";
 @register('canvas-screen')
 export default class CanvasScreen extends CustomComponent {
     canvas = document.createElement('canvas')
+
+    constructor() {
+        super()
+        // grid in canvas
+        
+    }
+
     render(): void {
         this.$.appendChild(this.canvas)
+
+        this.css`
+            canvas {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                background-color: #000;
+            }
+        `
     }
 }
