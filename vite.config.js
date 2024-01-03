13e5d45c5365131d9a301766a4c672338c3f80b6
Elí Maciel <tecno_eli@hotmail.com>
2024-01-03 02:08:14 -0600

    try config

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+    // Base public path when served in production.
+    base: '/gtp/',
+
+    // Build output configuration.
+    build: {
+        outDir: 'dist', // Output directory for production build.
+        assetsDir: 'assets', // Directory for static assets.
+        sourcemap: true, // Generate source maps for debugging.
+    },
+};
